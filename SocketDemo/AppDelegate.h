1b9c6763ff5d7b88879c8e022224fd86c7d0d321
rayshen <rayshen1012@gmail.com>
2015-06-12 11:11:51 +0800

    Initial Commit

---
+++ SocketDemo/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  SocketDemo
+//
+//  Created by shen on 15/6/12.
+//  Copyright (c) 2015年 shen. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ SocketDemo/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  SocketDemo
+//
+//  Created by shen on 15/6/12.
+//  Copyright (c) 2015年 shen. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
